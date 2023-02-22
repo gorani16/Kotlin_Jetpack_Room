7e61d7f99d057e2bab3a62d707de2e2d10c72e11
gorani16 <jgee0116@naver.com>
2023-02-23 04:00:56 +0900

    - Exercise Room (Entity, Dao) + Coroutine(some little)

--- app/src/main/java/com/gorani/jetpack_room/MainActivity.kt
+++ app/src/main/java/com/gorani/jetpack_room/MainActivity.kt
@@ -2,13 +2,20 @@ package com.gorani.jetpack_room
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 /**
- * Room
+ * Room 이란?
  * https://developer.android.com/training/data-storage/room?hl=ko
+ *
+ * Room 사용법
+ * https://developer.android.com/codelabs/android-room-with-a-view-kotlin?hl=ko#0
  */
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +24,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val db = TextDatabase.getDatabase(this)
 
+
+        CoroutineScope(Dispatchers.IO).launch {
+            db.textDao().insert(TextEntity(0,"Hello"))
+            val textData = db.textDao().getAllData()
+            Log.d("textData", textData.toString())
+        }
 
     }
 
