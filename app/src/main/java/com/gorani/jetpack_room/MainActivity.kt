5da066fe8b309122ebac89df61d37909575ea5c1
gorani16 <jgee0116@naver.com>
2023-02-24 04:25:12 +0900

    - Exercise Room (Entity, Dao) + Coroutine(some little)_2

--- app/src/main/java/com/gorani/jetpack_room/MainActivity.kt
+++ app/src/main/java/com/gorani/jetpack_room/MainActivity.kt
@@ -20,17 +20,50 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    private val inputArea: EditText by lazy {
+        findViewById(R.id.et_input)
+    }
+
+    private val btnInsert: Button by lazy {
+        findViewById(R.id.btn_insert)
+    }
+
+    private val btnGetData: Button by lazy {
+        findViewById(R.id.btn_get_data)
+    }
+
+    private val btnDelete: Button by lazy {
+        findViewById(R.id.btn_delete_all)
+    }
+
+    private val resultArea: TextView by lazy {
+        findViewById(R.id.tv_result)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val db = TextDatabase.getDatabase(this)
 
+        btnInsert.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                db.textDao().insert(TextEntity(0,inputArea.text.toString()))
+                inputArea.setText("")
+            }
+        }
 
-        CoroutineScope(Dispatchers.IO).launch {
-            db.textDao().insert(TextEntity(0,"Hello"))
-            val textData = db.textDao().getAllData()
-            Log.d("textData", textData.toString())
+        btnGetData.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val textData = db.textDao().getAllData()
+                Log.d("DB_Data : ", textData.toString())
+            }
+        }
+
+        btnDelete.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                db.textDao().deleteAllData()
+            }
         }
 
     }
